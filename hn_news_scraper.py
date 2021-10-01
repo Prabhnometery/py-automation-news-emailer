428913a9e811ad6eeacaab42dede34c49e94ffce
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2021-10-01 10:12:50 -0400

    sending email

--- hn_news_scraper.py
+++ hn_news_scraper.py
@@ -24,3 +24,29 @@ def extract_news(url):
         cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>')
                 if tag.text != 'More' else '')
     return(cnt)
+
+
+cnt = extract_news('https://news.ycombinator.com/')
+
+content += cnt
+content += ('<br>------</br>')
+content += ('<br><br>End of Message')
+
+# Sending an Email
+
+
+SERVER = 'smtp.gmail.com'
+PORT = 587
+FROM = 'prabhsimran29@gmail.com'
+TO = 'prabhsimran29@gmail.com'
+PASS = '****'
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' '
++ str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(content, 'html')
