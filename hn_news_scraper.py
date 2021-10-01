590f9ac3af50250f5ef8abb66f0ced1d0222b88b
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2021-10-01 09:52:58 -0400

    added extract function to extract the content from the HN webiste

---
+++ hn_news_scraper.py
@@ -0,0 +1,26 @@
+# HTTP Requests
+import requests
+
+# Web Scraping
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+
+now = datetime.datetime.now()
+
+content = ''
+
+
+def extract_news(url):
+    print("Extracting Hacker News Stories.....")
+    cnt = ''
+    cnt += ('<b>Hacker News Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(soup.find_all('td', attrs={'class': 'title', 'valign': ''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>')
+                if tag.text != 'More' else '')
+    return(cnt)
